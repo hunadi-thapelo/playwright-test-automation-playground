7295ccf046646da25c537a67a23075f182c2068d
hunadim <hmapulane@gmail.com>
2024-08-22 11:10:30 +0200

    adds except function and test assertion

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -1,6 +1,8 @@
-import {test} from '@playwright/test'
+import { test, expect } from '@playwright/test';
 
 test('the first test', async ({page})=> {
-   await page.goto('http://localhost:4200/')
+   await page.goto('http://localhost:4200/');
 
+   // Expect a url "to contain" a substring.
+   await expect(page).toHaveURL(/.*dashboard/);
 })
