ce56976e7dbf32d427f691bda4ab4b4fcf9c7d96
hunadim <hmapulane@gmail.com>
2024-10-02 17:38:54 +0200

    updates test title and remove debugging code

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -1,12 +1,10 @@
 import { test, expect } from '@playwright/test';
 
-test('the first test', async ({page})=> {
+test('Navigate to web page and open Form Layouts dashboard', async ({page})=> {
    await page.goto('http://localhost:4200/');
 
    // Expect a url "to contain" a substring.
    await expect(page).toHaveURL(/.*dashboard/);
    await page.getByText('Forms').click();
    await page.getByText('Form Layouts').click();
-
-   await page.pause()
 })
