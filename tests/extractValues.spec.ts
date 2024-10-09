84d5bdd8dc5e86e71120257aae08d0c2ccc30311
hunadim <hmapulane@gmail.com>
2024-10-09 11:27:29 +0200

    extracts input textfield values and asserts

--- tests/extractValues.spec.ts
+++ tests/extractValues.spec.ts
@@ -10,13 +10,39 @@ test.beforeEach(async ({page})=> {
 })
 
 test('Extract values and assert', async({page}) => {
+
     const blockForm = page.locator('nb-card').filter({hasText: "Block form"});
+    const usingTheGridForm = page.locator('nb-card').filter({hasText: "Using the Grid"});
+
     //extract button text and store in variable
     const buttonText = await blockForm.locator('button').textContent();
+    //extract radio button labels text and store in variable
+    const radioButtonLabels = await usingTheGridForm.locator('nb-radio').allTextContents();
+
+
 
     await test.step('Assert a single text value', async () => {
         //single text value
         expect(buttonText).toEqual('Submit');
     });
-    
+
+    await test.step('Extracts all text values and asserts with contains', async () => {
+        //all text values
+        expect(radioButtonLabels).toContain("Option 1");
+    });
+
+});
+
+test('Extract input textfield values and assert', async({page}) => {
+    const usingTheGridForm = page.locator('nb-card').filter({hasText: "Using the Grid"});
+    const inputEmailData = usingTheGridForm.getByRole('textbox', {name:"Email"});
+
+    //input values
+    await test.step('Extracts email input value and asserts', async () => {
+
+        await inputEmailData.fill('johndoe@email-example.com');
+        const emailDataStored = await inputEmailData.inputValue();
+        expect(emailDataStored).toEqual('johndoe@email-example.com');
+    });
+
 });
