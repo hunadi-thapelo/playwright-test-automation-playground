af362b14ee09a5ae73cfea2da35088b2105c3ebe
hunadim <hmapulane@gmail.com>
2025-12-16 09:27:53 +0200

    test: add test step to validate radio button is selected

--- tests/radioButtons.spec.ts
+++ tests/radioButtons.spec.ts
@@ -1,10 +1,13 @@
 import { test, expect } from './fixtures';
 
 
-test('Handling radio buttons', async({page}) =>{
 
-    const usingTheGridForm = page.locator('nb-card').filter({hasText: "Using the Grid"});
-    await usingTheGridForm.getByRole('radio', {name:'Option 1'}).check({force: true})
-    expect(usingTheGridForm.getByLabel('Option 1')).toBeChecked()
+    test('Validate that Option 1 radio button is checked', async({page}) =>{
 
-})
+        await test.step('Check Option 1 radio button', async () => {
+            const usingTheGridForm = page.locator('nb-card').filter({hasText: "Using the Grid"});
+            await usingTheGridForm.getByRole('radio', {name:'Option 1'}).check({force: true})
+            expect(usingTheGridForm.getByLabel('Option 1')).toBeChecked()
+        })
+    })
+
