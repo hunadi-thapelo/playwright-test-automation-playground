6db50017ba975f5bb0ea5999a719052f6c9eb294
hunadim <hmapulane@gmail.com>
2025-12-16 10:19:19 +0200

    test: assert radio button is checked and add const variables

--- tests/radioButtons.spec.ts
+++ tests/radioButtons.spec.ts
@@ -4,10 +4,14 @@ import { test, expect } from './fixtures';
 
     test('Validate that Option 1 radio button is checked', async({page}) =>{
 
+        const usingTheGridForm = page.locator('nb-card').filter({hasText: "Using the Grid"});
+        const radioButtonStatus = usingTheGridForm.getByRole('radio', {name:'Option 1'}).isChecked();
+
         await test.step('Check Option 1 radio button', async () => {
-            const usingTheGridForm = page.locator('nb-card').filter({hasText: "Using the Grid"});
-            await usingTheGridForm.getByRole('radio', {name:'Option 1'}).check({force: true})
-            expect(usingTheGridForm.getByLabel('Option 1')).toBeChecked()
+            await usingTheGridForm.getByRole('radio', {name:'Option 1'}).check({force: true});
+            expect(radioButtonStatus).toBeTruthy();
+            await expect(usingTheGridForm.getByLabel('Option 1')).toBeChecked();
+
         })
     })
 
