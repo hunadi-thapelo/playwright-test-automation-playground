dfdd2ee4b4df0f1b24cd046d61aea17149e92a09
hunadim <hmapulane@gmail.com>
2026-01-12 15:35:13 +0200

    test: verify background colour change after selection on dropdown menu

--- tests/listsDropdowns.spec.ts
+++ tests/listsDropdowns.spec.ts
@@ -4,20 +4,40 @@ import { test, expect } from './fixtures';
 
 test('Should select list-items from dropdown', async ({ page }) => {
 
+    const dropDownMenu = page.locator('ngx-header nb-select button')
+    const optionList = page.getByRole('list').locator('nb-option');
+    const header = page.locator('nb-layout-header');
+
+
     await test.step('Click on drop down menu', async () => {
-        const dropDownMenu = page.locator('ngx-header nb-select button')
         await dropDownMenu.click()
     })
 
     await test.step('Verify list items on the drop down menu', async () => {
-        const optionList = page.getByRole('list').locator('nb-option');
         await expect(optionList).toHaveText(["Light", "Dark", "Cosmic", "Corporate"])
+
+        await optionList.filter({ hasText: 'Cosmic' }).click()
     })
 
+    await test.step('Verify header background colour change as expected', async () => {
+        await expect(header).toHaveCSS('background-color', 'rgb(50, 50, 89)')
 
+        //create a plain object to loop through all background colour
+        const colours = {
+            "Light": "rgb(255, 255, 255)",
+            "Dark": "rgb(34, 43, 69)",
+            "Cosmic": "rgb(50, 50, 89)",
+            "Corporate": "rgb(255, 255, 255)"
+        }
 
+        await dropDownMenu.click()
 
-
-
-
+        //use an For in loop to select the colour and check rgb
+        for (const colour in colours) {
+            await optionList.filter({ hasText: colour }).click()
+            await expect(header).toHaveCSS('background-color', colours[colour])
+            if (colour != "Corporate")
+                await dropDownMenu.click()
+        }
+    })
 })
